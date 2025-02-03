1650f406a8acee120b491f03d2bd1085678891db
sreejaaa-cpu <komarraju.sreeja.s81@kalvium.community>
2025-02-03 11:48:00 +0530

    sreeja

--- server.js
+++ server.js
@@ -1,56 +1,47 @@
-// Boilerplate Code for HTTP Status Code API
+// Boilerplate Code for Virtual Assistant API
 const express = require('express');
 const app = express();
 
-/*
-Task:
-You need to create an API that helps users understand different HTTP status codes and their meanings.
+function message(){
 
-Requirements:
-1. Create a GET endpoint at "/status-info".
-2. The endpoint should accept a "code" as a query parameter (e.g., /status-info?code=200).
-3. Based on the status code provided, the API should respond with:
-   a. The status code.
-   b. A description of the status code.
+  let date =new Date()
+  let day =date.getDay()
 
-Example Responses:
-- For 200 (OK):
-  Request: /status-info?code=200
-  Response:
-  {
-    "status": 200,
-    "message": "OK: The request has succeeded. The meaning of this status depends on the HTTP method used."
+  if(day==1){
+    return {
+      "dayMessage": "Happy Monday! Start your week with energy!"
+    }
   }
-
-- For 404 (Not Found):
-  Request: /status-info?code=404
-  Response:
-  {
-    "status": 404,
-    "message": "Not Found: The server has not found anything matching the request URI. This is often caused by a missing page or resource."
+  else if(day==5){
+    return{
+      "dayMessage": "It's Friday! The weekend is near!"
+    }
   }
-
-- For 500 (Internal Server Error):
-  Request: /status-info?code=500
-  Response:
-  {
-    "status": 500,
-    "message": "Internal Server Error: The server encountered an unexpected condition that prevented it from fulfilling the request."
+  else{
+    return{
+      "dayMessage": "Have a wonderful day!"
+    }
   }
+}
 
-- For 400 (Bad Request):
-  Request: /status-info?code=400
-  Response:
-  {
-    "status": 400,
-    "message": "Bad Request: The server cannot process the request due to client-side errors (e.g., malformed syntax)."
-  }
 
-List of Status Codes to Handle:
-200, 201, 204, 400, 401, 403, 404, 405, 429, 500, 502, 503, 504
-*/
+
+app.get("/assistant/greet",async(req,res)=>{
+   try {
+      let user =req.query.name
+      console.log(user)
+       let mess = message()
+       console.log(mess)
+      let welcomeMessage ='Hello, ${user} Welcome to our assistant app!"'
+      mess.welcomeMessage =welcomeMessage
+      res.status(200).json(mess)
+    
+   } catch (error) {
+       res.status(500).json({status:false,message:"internal server error"})
+   }
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Status Code API is running on http://localhost:${PORT}`);
+    console.log(`Virtual Assistant API is running on http://localhost:${PORT}`);
 });
